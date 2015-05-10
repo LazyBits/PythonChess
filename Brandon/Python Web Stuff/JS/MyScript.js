b3677520c27aa2e783b1a64a944396d7ad4ef972
NuRelic <bnowlin6@gmail.com>
2015-05-10 14:41:43 -0700

    Brandon changes to add selecting, moving, and de-selecting

--- Brandon/Python Web Stuff/JS/MyScript.js
+++ Brandon/Python Web Stuff/JS/MyScript.js
@@ -1,23 +1,55 @@
-console.log($('.chess-row'));
-
-$('.chess-row').each(function() {
-	console.log(this);
-})
-
 var classHighlight = 'highlight'; 
+var classSelected = 'selected';
 
-var pieceNames = ["wR", "wK", "wB", "wQ", "wK", "wP", "bR", "bK", "bB", "bQ", "bK", "bP"]
+var blackPieces= ["bR", "bK", "bB", "bQ", "bK", "bP"];
+var whitePieces = ["wR", "wK", "wB", "wQ", "wK", "wP"];
+
+var moveComplete;
+
 $('.chess-field').click(function() {
-	
-		$(this).removeClass(classHighlight);	
-		if($(this).children('.chess-piece').html())
-		{
-			$(this).addClass(classHighlight);
-		}	
-	if($(classHighlight))
+	moveComplete = false;
+	if($('.chess-field').children('.chess-piece.selected').length == 1)
 	{
-		console.log($(classHighlight))
+		movePiece($(this));
 	}
-	else{
-	}
+	correctFocus($(this));	
 });
+
+var movePiece = function(chessField) {
+	var attackerName = $('.chess-piece.selected').html();
+	var defenderName = chessField.children('.chess-piece').html();	
+	if(attackerName)
+	{
+		var attackerColor = attackerName.charAt(0);
+		var defenderColor;
+		if(defenderName)
+		{
+			var defenderColor = defenderName.charAt(0);		
+		}
+		if(determineEnemy(attackerColor, defenderColor))
+		{			
+			chessField.children('.chess-piece').html(attackerName);
+			$('.chess-piece.selected').html("");
+		}
+	}
+	moveComplete = true;
+}
+
+var determineEnemy = function(attacker, defender) {
+	var result;
+	if(attacker != defender) 
+		result = true
+	else 
+		result = false
+	return result
+}
+
+var correctFocus = function(chessField) {	
+	$('.chess-field').removeClass(classHighlight);	
+	$('.chess-piece').removeClass(classSelected)
+	if(chessField.children('.chess-piece').html() != "" && moveComplete == false)
+	{
+		chessField.addClass(classHighlight);
+		chessField.children('.chess-piece').addClass(classSelected)
+	}		
+}
