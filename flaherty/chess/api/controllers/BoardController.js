2a3bb870013fb39cd966d1dc2d79485c3fe1e9b2
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2015-06-04 22:55:21 -0500

    Add simple api and json storage for single board

---
+++ flaherty/chess/api/controllers/BoardController.js
@@ -0,0 +1,11 @@
+/**
+ * BoardController
+ *
+ * @description :: Server-side logic for managing boards
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ flaherty/chess/api/models/Board.js
@@ -0,0 +1,16 @@
+/**
+* Board.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    pieces: {
+      type: 'json',
+      required: true
+    }
+  }
+};
